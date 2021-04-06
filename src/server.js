b0ca1dcafb4f3d6ba488316a91762d42713d3c9a
Isaac <isaacseligcoding@gmail.com>
2021-04-06 13:50:23 +0100

    updated server

--- src/server.js
+++ src/server.js
@@ -1,60 +1,21 @@
-const getAllPosts = async (req, res) => {
-  try {
-    const allPosts = await Post.find({});
-    res.status(200).send(allPosts);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-};
+require("./db/connection");
+const express = require("express");
+const { userRouter } = require("./routes/users");
+const { postRouter } = require("./routes/posts");
 
-const getUsersPosts = async (req, res) => {
-  try {
-    const allPosts = await Post.find({ author: req.params.user_id });
-    res.status(200).send(allPosts);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
-  }
-};
+const port = process.env.PORT || 5000;
+// init instance of express
+// middleware
+app.use(express.json());
 
-const addPost = async (req, res) => {
-  try {
-    const post = new Post(req.body);
-    post.author = req.params.user_id;
-    const returnedValue = await post.save();
+app.use(userRouter);
+app.use(postRouter);
 
-    res.status(201).send(returnedValue);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
+// routes/endpoints
+app.get("/health", (req, res) => {
+  res.send({ message: "API is working correctly" });
+});
 
-const updatePost = async (req, res) => {
-  try {
-    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    console.log(post);
-    res.status(200).send(post);
-  } catch (error) {
-    res.status(404).send({ message: "post not found" });
-  }
-};
-
-const deletePost = async (req, res) => {
-  try {
-    const post = await Post.findByIdAndDelete(req.params.id);
-    res.status(200).send(post);
-  } catch (error) {
-    res.status(404).send({ message: "post not found" });
-  }
-};
-
-module.exports = {
-  getAllPosts,
-  getUsersPosts,
-  addPost,
-  updatePost,
-  deletePost,
-};
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
